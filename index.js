e1eb63e47468a47091a8650ad092b84748db508a
loukhai <loukhairabia@gmail.com>
2024-12-09 20:22:46 +0100

    chore: fake update

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ console.log("hello semantic release and github actions");
 const branch = "pnpm";
 const wow = true;
 
-if (wow) {
-  console.log(`you are in ${branch} branch`);
-}
+console.log(`you are in ${branch} branch`);

bee10f6e27ec3f578d037b98fc4f2ecffca54253
loukhai <loukhairabia@gmail.com>
2024-12-09 13:25:00 +0100

    feat: new const wow

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 console.log("hello semantic release and github actions");
 
 const branch = "develop";
-console.log(`you are in ${branch} branch`);
+const wow = true;
+
+if (wow) {
+  console.log(`you are in ${branch} branch`);
+}
